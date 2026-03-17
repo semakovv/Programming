b72d89287e0c95b4fd8d717559b91bac23d08f44
semakovv <semakov.v@edu.narfu.ru>
2026-03-17 23:40:11 +0300

    Update ЛР14_Прог_Семаков.В.В_151518.py

--- ЛР14_Прог_Семаков.В.В_151518.py
+++ ЛР14_Прог_Семаков.В.В_151518.py
@@ -1,103 +1,33 @@
-# 1 #
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("intxt", help="input text in txt file")
-# args = parser.parse_args()
-# with open("./Task1_output.txt", "w") as w:
-#     w.write(args.intxt)
-# w.close()
-# 2 #
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("intxt", help="input text in txt file")
-# parser.add_argument("-f", "--file", help="input name the txt file")
-# args = parser.parse_args()
-# with open("./Task1_output.txt", "w") as w:
-#     w.write(args.intxt)
-# w.close()
-# if args.file:
-#     with open(f"./{args.file}", "w") as w:
-#         w.write(args.intxt)
-#     w.close()
-# 3 #
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("intxt", help="input text in txt file")
-# parser.add_argument("-f", "--file", help="input name the txt file")
-# args = parser.parse_args()
-# with open("./Task1_output.txt", "w") as w:
-#     w.write(args.intxt)
-# w.close()
-# if args.file == "":
-#     print("Error")
-# else:
-#     with open(f"./{args.file}.txt", "w") as w:
-#         w.write(args.intxt)
-#     w.close()
-# 4 # как сделать валидацию?
-# import argparse
-# try:    
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("intxt", help="input text in txt file")
-#     parser.add_argument("-f", "--file", help="input name the txt file")
-#     parser.add_argument("-r", "--read", help="read the txt file")
-#     args = parser.parse_args()
-#     with open("./Task1_output.txt", "w") as w:
-#         w.write(args.intxt)
-#     w.close()
-#     if args.file:
-#         with open(f"{args.file}", "w") as w:
-#             w.write(args.intxt)
-#         w.close()
-#     if args.read:
-#         with open(f"{args.read}") as r:
-#             s = r.read()
-#         print(s)
-#         r.close()
-# except Exception as e:
-#     print(f"Error: {e}")
-# 5 # документация?
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("intxt", help="input text in txt file")
-# parser.add_argument("-f", "--file", help="input name the txt file")
-# parser.add_argument("-r", "--read", help="read the txt file")
-# parser.add_argument("-c", "--count", help="output count words in txt file")
-# args = parser.parse_args()
-# count = 1
-# with open("./Task1_output.txt", "w") as w:
-#     w.write(args.intxt)
-# w.close()
-# if args.file == "":
-#     print("Error")
-# else:
-#     with open(f"./{args.file}.txt", "w") as w:
-#         w.write(args.intxt)
-#     w.close()
-# if args.read:
-#     with open(f"./{args.read}.txt") as r:
-#         s = r.read()
-#     print(s)
-#     r.close()
-# if args.count:    
-#     with open(f"./{args.count}.txt") as r:
-#         s = r.read()
-#     s = s.split()
-#     print(len(s))
-#     r.close()
-# final
 import argparse
+
 parser = argparse.ArgumentParser()
-parser.add_argument("intxt", help="input text in txt file")
-parser.add_argument("-f", "--file", help="input name the txt file", default="Task1_output.txt")
+
+parser.add_argument("intxt", type=str, help="Приложение для создания файла txt", )
+parser.add_argument("-f", "--file", type=str, help="Имя файла txt, по умолчанию Task1_output.txt", default="Task1_output")
+parser.add_argument("-r", "--read", type=str, help="Чтение файла txt, по умолчанию Task1_output.txt", default="Task1_output")
+parser.add_argument("-c", "--count", type=str, help="Кол-во слов файла txt, по умолчанию Task1_output.txt", default="Task1_output")
+
 args = parser.parse_args()
-with open("Task1_output.txt", "w") as w:
-    w.write(args.intxt)
-w.close()
+
+if args.intxt:
+    with open("./Task1_output.txt", "w") as w:
+        w.write(f"{args.intxt}")
+    w.close()
 try:
     if args.file:
-        with open(f"{args.file}", "w") as w:
-            w.write(args.intxt)
+        with open(f"./{args.file}.txt", "w") as w:
+            w.write(f"{args.intxt}")
         w.close()
+    if args.read:
+        with open(f"./{args.read}.txt", "r") as r:
+            read = r.read()
+        print(read)
+        r.close()
+    if args.count:
+        with open(f"./{args.count}.txt", "r") as r:
+            read = r.read()
+        lst = read.split()
+        print(len(lst))
 except Exception as e:
-    print("Error srjhwrjwwhdqwhj")
+    print("Error", e)
+
