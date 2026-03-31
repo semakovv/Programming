0b3207d84b6f49ac0cfb1b05369dee1589fa219e
Versus <semakov.v@edu.narfu.ru>
2026-03-31 19:34:13 +0300

    Update ЛР15.3_Прог_Семаков.В.В_151518.py

--- ЛР15.3_Прог_Семаков.В.В_151518.py
+++ ЛР15.3_Прог_Семаков.В.В_151518.py
@@ -1,15 +1,19 @@
 import subprocess
+flag = True
 ip_area = input("Введите диапозон адрессов: ")
 print(ip_area)
-try:
-    lst_ips = ip_area.split("-")
-    print(lst_ips)
-    lst_octets = []
-    for i in lst_ips:
-        lst_octets.append(i.split("."))
-    # lst_octets = [lst_ips[i].split(".") for i in lst_ips] #как сдалить через генерацию???
-    print(lst_octets)
-    for i in lst_octets:
-        
-except Exception as e:
-    print("Error", e)
+lst_ips = ip_area.split("-")
+print(lst_ips)
+lst_octets = []
+for i in lst_ips:
+    lst_octets.append(i.split("."))
+# lst_octets = [lst_ips[i].split(".") for i in lst_ips] #как сдалить через генерацию???
+print(lst_octets)
+for i in lst_octets:
+    for j in i:
+        if j.isdigit() and 0 <= int(j) <= 255:
+            flag = True
+        else:
+            flag = False
+            break
+            print("Неверный ввод")
