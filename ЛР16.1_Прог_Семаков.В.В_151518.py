0db205b7bdc3b9027d3503cca3bb922f5c79cb1c
semakovv <semakov.v@edu.narfu.ru>
2026-04-20 11:42:31 +0300

    Update ЛР16.1_Прог_Семаков.В.В_151518.py

--- ЛР16.1_Прог_Семаков.В.В_151518.py
+++ ЛР16.1_Прог_Семаков.В.В_151518.py
@@ -11,17 +11,35 @@ class Link:
             print(p.data, end = " ")
             p = p.next
         print()
+    def AddFirst(self, a = None):
+        f = Node(a)
+        f.next = self.head
+        self.head = f
+    def AddEnd(self, a = None):
+        e = Node(a)
+        e.next = None
 
 testlist = Link()
 
 testlist.head = Node("1")
 
-testlist.head.next = e2
-
 e2 = Node("2")
-
-e2.next = e3
 
 e3 = Node("3")
 
+testlist.head.next = e2
+
+e2.next = e3
+
 testlist.PrintList()
+
+testlist.AddFirst("4")
+
+testlist.PrintList()
+
+e4 = testlist.AddEnd("5")
+
+e3.next = e4
+
+testlist.PrintList()
+
