eb6f6cd7194b080c370b1edec0acc7a568ef451c
Versus <semakov.v@edu.narfu.ru>
2026-05-07 22:05:03 +0300

    Update ЛР16.2_Прог_Семаков.В.В_151518.py

---
+++ ЛР16.2_Прог_Семаков.В.В_151518.py
@@ -0,0 +1,75 @@
+from collections import deque
+import re
+
+def operPattern(ST):
+    ptrn = re.findall(r"[+-/*]{1} \d+", ST)
+    if (ptrn):
+        return ST
+    
+def oper(ST, TOTAL):
+
+    op = ST[0]
+    num = int(ST[2::])
+
+    if op == "+":
+        TOTAL += num
+    if op == "-":
+        TOTAL -= num
+    if op == "*":
+        TOTAL *= num
+    if op == "/":
+        TOTAL /= num
+    return op, num, TOTAL
+
+def unOper(ST, TOTAL):
+
+    op = ST[0]
+    num = int(ST[2::])
+
+    if op == "+":
+        TOTAL -= num
+    if op == "-":
+        TOTAL += num
+    if op == "*":
+        TOTAL /= num
+    if op == "/":
+        TOTAL *= num
+    return op, num, TOTAL
+
+
+
+
+st = "+ 0"
+total = 0
+stackForOper = deque()
+stackForUnOper = deque()
+
+
+
+while st != "stop":
+    st = input()
+    print(total)
+
+    if (operPattern(st)):
+        print(True)
+        stackForOper.append(st)
+        oper(st, total)
+        continue
+
+    if st == "undo":
+        save = stackForOper.pop()
+        stackForUnOper.append(save)
+        unOper(save, total)
+        continue
+
+    if st == "redo":
+        save = stackForUnOper.pop()
+        oper(save, total)
+        continue
+
+
+
+
+
+
+
