efccbba0d8db94b2dbb593768b234499ea5e37fa
semakovv <semakov.v@edu.narfu.ru>
2026-05-27 17:11:17 +0300

    Update .py.py.py

--- .py.py.py
+++ .py.py.py
@@ -1,11 +1,3 @@
-a = 1
-
-def func(NUM):
-    total = NUM * 2
-    return total
-print(func(a))
-
-def func_1(NUM):
-    NUM * 2
-    return NUM
-print(func_1(a))
+lib = {1: 2, 0: 1, -1: 1, 2: 1}
+for i in lib:
+    print(i)
