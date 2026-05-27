638bf9cb3595d7178cfde9d7066b4bfa2b3d60f9
semakovv <semakov.v@edu.narfu.ru>
2026-05-27 08:30:26 +0300

    Update ЛР16.3_Прог_Семаков.В.В_151518.py

--- ЛР16.3_Прог_Семаков.В.В_151518.py
+++ ЛР16.3_Прог_Семаков.В.В_151518.py
@@ -1,3 +1,30 @@
 from collections import deque
 
-que = deque()
+def addInQueue(Q):
+    """
+    Добавление чисел в очередь
+    """
+    print("Введите последовательность")
+    NUM = 0
+    while not(str(NUM).isdigit()):
+        NUM = int(input())
+        Q.append(NUM)
+    return Q
+
+def sortQueue(Q1, Q2):
+    """
+    Объединение, сортировка, печать очередей
+    """
+    QLAST = list(Q1 + Q2)
+    QLAST.sort()
+    QLAST = deque(QLAST)
+    print(QLAST)
+
+q1 = deque()
+q2 = deque()
+
+q1 = addInQueue(q1)
+q2 = addInQueue(q2)
+sortQueue(q1, q2)
+
+
