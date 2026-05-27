1c823cb6c82f5baef9d3cd6e6e68e8e62e2400ee
semakovv <semakov.v@edu.narfu.ru>
2026-05-27 09:13:53 +0300

    Update ЛР16.3_Прог_Семаков.В.В_151518.py

--- ЛР16.3_Прог_Семаков.В.В_151518.py
+++ ЛР16.3_Прог_Семаков.В.В_151518.py
@@ -1,14 +1,39 @@
+# from collections import deque
+
+# def addInQueue(NUMS, Q):
+#     """
+#     Добавление чисел в очередь
+#     """
+#     for i in NUMS:
+#         Q.append(int(i))
+#     return Q
+
+# def sortQueue(Q1, Q2):
+#     """
+#     Объединение, сортировка, печать очередей
+#     """
+#     QLAST = list(Q1 + Q2)
+#     QLAST.sort()
+#     QLAST = deque(QLAST)
+#     print(QLAST)
+
+# q1 = deque()
+# q2 = deque()
+# nums1 = input()
+# nums2 = input()
+
+# q1 = addInQueue(nums1, q1)
+# q2 = addInQueue(nums2, q2)
+# sortQueue(q1, q2)
+
 from collections import deque
 
-def addInQueue(Q):
+def addInQueue(NUMS, Q):
     """
     Добавление чисел в очередь
     """
-    print("Введите последовательность")
-    NUM = 0
-    while not(str(NUM).isdigit()):
-        NUM = int(input())
-        Q.append(NUM)
+    for i in NUMS:
+        Q.append(int(i))
     return Q
 
 def sortQueue(Q1, Q2):
@@ -22,9 +47,11 @@ def sortQueue(Q1, Q2):
 
 q1 = deque()
 q2 = deque()
+nums1 = input()
+nums2 = input()
 
-q1 = addInQueue(q1)
-q2 = addInQueue(q2)
+q1 = addInQueue(nums1, q1)
+q2 = addInQueue(nums2, q2)
 sortQueue(q1, q2)
 
 
